6568d5916caecbf3013ce1e8f598d4a60731f008
Anna <piladzisi@gmail.com>
2019-04-24 15:56:06 +0300

    sorting into sections by first letter + sectionIndexTitles

--- Contacts/ContactListController.swift
+++ Contacts/ContactListController.swift
@@ -8,9 +8,32 @@
 
 import UIKit
 
+extension Contact {
+    var firstLetterForSort: String {
+       return  String(firstName.first!).uppercased()
+    }
+}
+
+extension  ContactsSource {
+    static var sortedUniqueFirstLetters: [String] {
+        let firstLetters = contacts.map { $0.firstLetterForSort }
+        let uniqueFirstLetters = Set(firstLetters)
+        return Array(uniqueFirstLetters).sorted()
+    }
+    
+    static var sectionedContacts: [[Contact]] {
+       return sortedUniqueFirstLetters.map { firstLetter in
+            let filteredContacts = contacts.filter { $0.firstLetterForSort == firstLetter }
+                return filteredContacts.sorted(by: { $0.firstName < $1.firstName })
+            }
+        }
+    }
+
+
 class ContactListController: UITableViewController {
     
-    var contacts = ContactsSource.contacts
+   var sections = ContactsSource.sectionedContacts
+    let sectionTitles = ContactsSource.sortedUniqueFirstLetters
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +41,26 @@ class ContactListController: UITableViewController {
     }
     //MARK: - Data Source
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+   override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+       return sectionTitles
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return contacts.count
+       return sections[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
         
-        let contact = contacts[indexPath.row]
+        let contact = sections[indexPath.section][indexPath.row]
         
         cell.textLabel?.text = contact.firstName
         cell.imageView?.image = contact.image
@@ -42,7 +73,7 @@ class ContactListController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showContact" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let contact = contacts[indexPath.row]
+                let contact = sections[indexPath.section][indexPath.row]
                 
                 guard let navigationController = segue.destination as? UINavigationController, let contactDetailController = navigationController.topViewController as? ContactDetailController else { return }
             
