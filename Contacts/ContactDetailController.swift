7d0ea57990ea63f0bffe72826e857f11b8906799
Anna <piladzisi@gmail.com>
2019-04-24 14:07:17 +0300

    detail view with table view sections

--- Contacts/ContactDetailController.swift
+++ Contacts/ContactDetailController.swift
@@ -16,21 +16,5 @@ class ContactDetailController: UITableViewController {
         super.viewDidLoad()
 
     
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
- 
-
-   
+    }   
 }
