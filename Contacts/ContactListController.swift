44ae3494450f9e8b7e5518f729b39d5f6b7f6f1f
Anna <piladzisi@gmail.com>
2019-04-25 17:21:45 +0300

    refactoring code, star bug

--- Contacts/ContactListController.swift
+++ Contacts/ContactListController.swift
@@ -32,45 +32,12 @@ extension  ContactsSource {
 
 class ContactListController: UITableViewController {
     
-   var sections = ContactsSource.sectionedContacts
-    let sectionTitles = ContactsSource.sortedUniqueFirstLetters
+    let dataSource = ContactsDataSource(sectionedData: ContactsSource.sectionedContacts, sectionTitles: ContactsSource.sortedUniqueFirstLetters)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-    //MARK: - Data Source
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
-    }
-    
-   override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-       return sectionTitles
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return sections[section].count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let contactCell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as? ContactCell else { fatalError() }
         
-        let contact = sections[indexPath.section][indexPath.row]
-        
-        contactCell.profileImageView.image = contact.image
-        contactCell.profileImageView.layer.cornerRadius = 30
-        contactCell.nameLabel.text = contact.firstName
-        contactCell.cityLabel.text = contact.city
-        
-        if contact.isFavorite {
-            contactCell.favoriteView.image = #imageLiteral(resourceName: "Star")
-        }
-        return contactCell
+        tableView.dataSource = dataSource
     }
     
     // MARK: - Table View Delegate
@@ -86,7 +53,7 @@ class ContactListController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showContact" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let contact = sections[indexPath.section][indexPath.row]
+                let contact = dataSource.object(at: indexPath)
                 
                 guard let navigationController = segue.destination as? UINavigationController, let contactDetailController = navigationController.topViewController as? ContactDetailController else { return }
             
@@ -107,22 +74,12 @@ extension Contact: Equatable {
 
 extension ContactListController : ContactDetailControllerDelegate {
     func didMarkAsFavoriteContact(_ contact: Contact) {
-        var outerIndex: Int? = nil
-        var innerIndex: Int? = nil
+        guard let indexPath = dataSource.indexPath(for: contact) else { return }
         
-        for (index, contacts) in sections.enumerated() {
-            if let indexOfContact = contacts.firstIndex(of: contact) {
-                outerIndex = index
-                innerIndex = indexOfContact
-                break
-            }
-        }
-        if let outerIndex = outerIndex, let innerIndex = innerIndex {
-            var favoriteContact = sections[outerIndex][innerIndex]
-            favoriteContact.isFavorite = true
-            sections[outerIndex][innerIndex] = favoriteContact
-            
-            tableView.reloadData() 
-        }
+        var favoriteContact = dataSource.object(at: indexPath)
+        favoriteContact.isFavorite  = true
+        
+        dataSource.updateContact(favoriteContact, at: indexPath)
+        tableView.reloadData()
     }
 }
--- Contacts/ContactsDataSource.swift
+++ Contacts/ContactsDataSource.swift
@@ -10,5 +10,63 @@ import Foundation
 import UIKit
 
 class ContactsDataSource: NSObject, UITableViewDataSource {
+    private var sectionedData: [[Contact]]
+    let sectionTitles: [String]
     
+    init(sectionedData: [[Contact]], sectionTitles: [String]) {
+        self.sectionedData = sectionedData
+        self.sectionTitles = sectionTitles
+        
+        super.init()
+    }
+    //MARK: - Data Source
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+   func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sectionTitles
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionedData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionedData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let contactCell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as? ContactCell else { fatalError() }
+        
+        let contact = sectionedData[indexPath.section][indexPath.row]
+        
+        contactCell.profileImageView.image = contact.image
+        contactCell.profileImageView.layer.cornerRadius = 30
+        contactCell.nameLabel.text = contact.firstName
+        contactCell.cityLabel.text = contact.city
+        
+        if contact.isFavorite {
+            contactCell.favoriteView.image = #imageLiteral(resourceName: "Star")
+        }
+        return contactCell
+    }
+    
+    //MARK: - Helper Methods
+    
+    func object(at indexPath: IndexPath) -> Contact {
+        return sectionedData[indexPath.section][indexPath.row]
+    }
+    func indexPath(for contact: Contact) -> IndexPath? {
+        for (index, contacts) in sectionedData.enumerated() {
+            if let indexOfContact = contacts.index(of: contact) {
+                return IndexPath(row: indexOfContact, section: index)
+            }
+        }
+        return nil
+    }
+    func updateContact(_ contact: Contact, at indexPath: IndexPath) {
+        sectionedData[indexPath.section][indexPath.row] = contact
+    }
 }
